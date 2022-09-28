87e7ca70651945f257067c456864fc54931d015c
Juhasz Csenge <csenge799@gmail.com>
2022-09-28 14:38:14 +0200

    weboldal kinezet

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
+//import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './style.css';
+ let link;
+ let pixel;
+ let number;
 
+
+function submit(){
+     link = document.getElementById("linkInput").value;
+    document.getElementById("showPic").src = link; 
+    };
+
+//let picPixel = parseFloat( document.getElementById(pixelInput).value);
+//let framePixel = parseFloat( document.getElementById(frameInput).value);
