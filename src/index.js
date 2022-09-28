effd184b6a0285056cb0db6849dfc4f80a2015d5
Juhasz Csenge <csenge799@gmail.com>
2022-09-28 14:50:04 +0200

    mai ora vege

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 //import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 import 'bootstrap/dist/js/bootstrap.bundle.js'
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import './style.css';
  let link;
- let pixel;
- let number;
+ let pixel = document.getElementById('pixelInput');
+ let number = document.getElementById('frameInput');
 
 
-function submit(){
-     link = document.getElementById("linkInput").value;
-    document.getElementById("showPic").src = link; 
+    function submit(){
+         link = document.getElementById("linkInput");
+        document.getElementById("showPic").src = link; 
     };
+    
+    function darkmode(){
+        pixel = pixel.style.width(document.getElementById(showPic))
+    }
+    if(){
+
+    };
+
+
+    
 
 //let picPixel = parseFloat( document.getElementById(pixelInput).value);
 //let framePixel = parseFloat( document.getElementById(frameInput).value);
